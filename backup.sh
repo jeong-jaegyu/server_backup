6f411efc25263bacfd465cd55a43071e46075f20
jahu50d <>
2022-01-19 16:45:05 +0000

    backup-comments

--- backup.sh
+++ backup.sh
@@ -1,10 +1,20 @@
 #! /bin/bash
 
 server_name=${PWD##*/}
+
 date_time=$(date +%d%m%Y-%H%M)
 file_name="${server_name}_${date_time}"
 
-tar -cf ${file_name}.tar $(cat important_files)
-gzip -v9 ${file_name}.tar
+echo 'Executing backup for' ${server_name}
+echo 'Current DateTime is' ${date_time}
 
+echo "Files adding to archive:" $(cat important_files)
+tar -cf ${file_name}.tar $(cat important_files)
+echo "Finished tar..."
+
+echo "GZipping..."
+gzip -v9 ${file_name}.tar
+echo "GZip succesful..."
+
+echo "Moving tar to backup dir"
 mv ${file_name}.tar.gz /srv/backups/minecraft/${server_name}/${file_name}.tar.gz
